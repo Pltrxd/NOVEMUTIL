c3c58b9a83b4a7cfe9d7b5800ef79a9eb97c81a8
Pltrxd <117510712+Pltrxd@users.noreply.github.com>
2024-06-04 15:06:04 +0000

    v0

---
+++ commands/utility/eval.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "eval",
+    code: `
+    $eval[$message]
+    $onlyForIDs[$clientOwnerIds;You can't use that command!]
+  `,
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+const { AoiClient, LoadCommands } = require("aoi.js");
+
+const client = new AoiClient({
+  token: process.env['TOKEN'],
+  prefix: "f!",
+  intents: ["MessageContent", "Guilds", "GuildMessages"],
+  events: ["onMessage", "onInteractionCreate"],
+  database: {
+    type: "aoi.db",
+    db: require("@akarui/aoi.db"),
+    dbType: "KeyValue",
+    tables: ["main"],
+    securityKey: "0350619aef072c3905558ff3f821d531",
+  }
+});
+
+client.status({
+    name: "$guildCount Servers 🧰",
+    type: "WATCHING",
+    time: 12
+});
+
+const loader = new LoadCommands(client);
+loader.load(client.cmd, "./commands")
